b851ae61c0ce33a369949421203d65562f9169b9
mlehau21 <mle.hau21@gmail.com>
2023-02-07 11:08:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controller/contact.controller.js
@@ -0,0 +1,27 @@
+exports.screate = (req, res) => {
+    res.send({ message: "screate handler"});
+};
+
+exports.findAll = (req, res) => {
+    res.send({ message: "findAll handler"});
+};
+
+exports.findOne = (req, res) => {
+    res.send({ message: "findOne handler"});
+};
+
+exports.update = (req, res) => {
+    res.send({ message: "update handler"});
+};
+
+exports.delete = (req, res) => {
+    res.send({ message: "delete handler"});
+};
+
+exports.deleteAll = (req, res) => {
+    res.send({ message: "deleteAll handler"});
+};
+
+exports.findAllFavorire = (req, res) => {
+    res.send({ message: "findAllFavorite handler"});
+};
